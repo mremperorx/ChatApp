eb050351d875314cdda7e81c0c722e1dbf5e5bd3
mremperorx <Majedalnasr7@gmail.com>
2023-01-28 04:18:22 +0200

    Nothing!

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,6 +8,17 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+        <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+                float: isMyMessage ? 'right' : 'left',
+                backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+            }}
+        />
+    ));
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
